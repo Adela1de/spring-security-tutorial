59c99ae97fd7a15c8c42bb38ee2a3be6ae484a73
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-05-18 09:50:51 -0300

    User registration

--- spring-security-tutorial/src/main/java/com/luizaugusto/springsecuritytutorial/controllers/RegistrationController.java
+++ spring-security-tutorial/src/main/java/com/luizaugusto/springsecuritytutorial/controllers/RegistrationController.java
@@ -3,21 +3,21 @@ package com.luizaugusto.springsecuritytutorial.controllers;
 import com.luizaugusto.springsecuritytutorial.model.UserModel;
 import com.luizaugusto.springsecuritytutorial.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
+@CrossOrigin("*")
 public class RegistrationController {
 
     private final UserService userService;
+
 
     @PostMapping("/register")
     public String registerUser(@RequestBody UserModel userModel)
     {
         var user = userService.registerUser(userModel);
-        return null;
-
+        return "success";
     }
 }
--- spring-security-tutorial/src/main/java/com/luizaugusto/springsecuritytutorial/services/UserService.java
+++ spring-security-tutorial/src/main/java/com/luizaugusto/springsecuritytutorial/services/UserService.java
@@ -4,6 +4,7 @@ import com.luizaugusto.springsecuritytutorial.entitites.User;
 import com.luizaugusto.springsecuritytutorial.model.UserModel;
 import com.luizaugusto.springsecuritytutorial.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +13,18 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     public User registerUser(UserModel userModel)
     {
         var user = new User();
         user.setEmail(userModel.getEmail());
         user.setFirstName(userModel.getFirstName());
         user.setLastName(userModel.getLastName());
-        user.setPassword(userModel.getPassword());
+        user.setPassword(passwordEncoder.encode(userModel.getPassword()));
         user.setRole("USER");
-        return null;
+
+        userRepository.save(user);
+        return user;
     }
 }
